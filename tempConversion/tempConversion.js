9af148d3b7e6a57806849f9c25e672461916ba58
xxerror500xx <cliff.minks@gmail.com>
2018-02-08 14:52:09 -0600

    Add tempConversion solution

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
-var ftoc = function() {
-  
-}
+var ftoc = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * 5/9)*10) / 10;
+};
 
-var ctof = function() {
-  
-}
+var ctof = function(celsius) {
+  return Math.round(((celsius * 9/5) + 32)*10) / 10;
+};
 
 module.exports = {
   ftoc,
   ctof
-}
+};
