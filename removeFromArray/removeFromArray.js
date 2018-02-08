cbf44bb5e13b548073413445769207bb49a0cd06
xxerror500xx <cliff.minks@gmail.com>
2018-02-07 21:20:57 -0600

    Add removeFromArray solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 var removeFromArray = function() {
+  var oArr = arguments[0];
+  for (var i = 1; i < arguments.length; i++) {
+    var index = oArr.indexOf(arguments[i]);
+    if (index !== -1) {
+      oArr.splice(index, 1);
+    }
+  }
+  return oArr;
+};
 
-}
-
-module.exports = removeFromArray
+module.exports = removeFromArray;
