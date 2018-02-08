ce54225c0e6287a994bc2cacccba21b0623405ac
xxerror500xx <cliff.minks@gmail.com>
2018-02-07 21:32:36 -0600

    Add ES6 ECMA2015 solution of removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,12 +1,18 @@
-var removeFromArray = function() {
-  var oArr = arguments[0];
-  for (var i = 1; i < arguments.length; i++) {
-    var index = oArr.indexOf(arguments[i]);
-    if (index !== -1) {
-      oArr.splice(index, 1);
-    }
-  }
-  return oArr;
+// var removeFromArray = function() {
+//   var oArr = arguments[0];
+//   for (var i = 1; i < arguments.length; i++) {
+//     var index = oArr.indexOf(arguments[i]);
+//     if (index !== -1) {
+//       oArr.splice(index, 1);
+//     }
+//   }
+//   return oArr;
+// };
+
+// ECMA2015 useage of the spread operator and a more compact filter on value
+var removeFromArray = function(...arguments) {
+  var array = arguments[0];
+  return array.filter(val => !arguments.includes(val));
 };
 
 module.exports = removeFromArray;
