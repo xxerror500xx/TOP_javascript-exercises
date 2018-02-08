813cac29f0d29204f71d61bb3103e825b8226d38
xxerror500xx <cliff.minks@gmail.com>
2018-02-07 18:52:27 -0600

    add non ECMA2015 alternate function without polyfill

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,13 +1,26 @@
 var repeatString = function(str, num) {
-  if (num > 0) {
-    for (var i = 0; i < num; i++) {
-      return str.repeat(num);
-    }
-  }else if (num === 0) {
-    return '';
-  }else {
+  var newStr = '';
+  if (num < 0) {
     return 'ERROR';
+  }else {
+    for (var i = 0; i < num; i++) {
+      newStr += str;
+    }
   }
+  return newStr;
+
 }
+// This is an ECMA2015 modern version 
+// var repeatString = function(str, num) {
+//   if (num > 0) {
+//     for (var i = 0; i < num; i++) {
+//       return str.repeat(num);
+//     }
+//   }else if (num === 0) {
+//     return '';
+//   }else {
+//     return 'ERROR';
+//   }
+// }
 
 module.exports = repeatString
