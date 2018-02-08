f6010c0f2309c41aa264bfbcc5d04cb1d699045d
xxerror500xx <cliff.minks@gmail.com>
2018-02-07 18:34:09 -0600

    add repeat solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-var repeatString = function() {
-
+var repeatString = function(str, num) {
+  if (num > 0) {
+    for (var i = 0; i < num; i++) {
+      return str.repeat(num);
+    }
+  }else if (num === 0) {
+    return '';
+  }else {
+    return 'ERROR';
+  }
 }
 
 module.exports = repeatString
