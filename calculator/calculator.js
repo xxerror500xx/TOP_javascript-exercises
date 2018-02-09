1eefb67763f0b115c0af30af25c8d3da2739f1a1
xxerror500xx <cliff.minks@gmail.com>
2018-02-09 12:46:50 -0600

    Add refactor for fewer lines of code

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,64 +1,68 @@
-function add() {
-  var total = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    total += arguments[i];
-  }
-  return total;
+function add(n1, n2) {
+  return n1 + n2
+  // Long version below
+  // var total = 0;
+  // for (var i = 0; i < arguments.length; i++) {
+  //   total += arguments[i];
+  // }
+  // return total;
 }
 
 function subtract(n1, n2) {
   return n1 - n2;
 }
 
-function sum() {
-  if (Array.isArray(arguments[0])) {
-    var values = arguments[0];
-
-    if (values.length === 0) {
-      return 0;
-    }else {
-      var total = 0;
-      for (var i = 0; i < values.length; i++) {
-        total += values[i];
-      }
-      return total;
-    }
-  }
+function sum(array) {
+  return array.reduce((current, total) => total + current, 0);
+  // if (Array.isArray(arguments[0])) {
+  //   var values = arguments[0];
+  //
+  //   if (values.length === 0) {
+  //     return 0;
+  //   }else {
+  //     var total = 0;
+  //     for (var i = 0; i < values.length; i++) {
+  //       total += values[i];
+  //     }
+  //     return total;
+  //   }
+  // }
 }
 
-function multiply() {
-  if (Array.isArray(arguments[0])) {
-    var values = arguments[0];
-
-    if (values.length === 0) {
-      return 0;
-    }else {
-      var total = 1;
-      for (var i = 0; i < values.length; i++) {
-        total *= values[i];
-      }
-      return total;
-    }
-  }
+function multiply(array) {
+  return array.reduce((current, total) => total * current, 1);
+  // if (Array.isArray(arguments[0])) {
+  //   var values = arguments[0];
+  //
+  //   if (values.length === 0) {
+  //     return 0;
+  //   }else {
+  //     var total = 1;
+  //     for (var i = 0; i < values.length; i++) {
+  //       total *= values[i];
+  //     }
+  //     return total;
+  //   }
+  // }
 }
 
 function power(n1, n2) {
-  var total = n1;
-  for (var i = 1; i < n2; i++) {
-    total *= n1;
-  }
-  return total;
+  return Math.pow(n1, n2);
+  // var total = n1;
+  // for (var i = 1; i < n2; i++) {
+  //   total *= n1;
+  // }
+  // return total;
 }
 
 function factorial(n1) {
   var total = 1;
-  if (n1 === 0 || n1 === 1) {
-    return 1;
-  }
+  if (n1 === 0) return 1;
+
   if (n1 === 2) {
     return 2;
-  }else {
-    for (var i = 1; i <= n1 ; i++) {
+  } else {
+    for (var i = 1; i <= n1; i++) {
       total *= i;
     }
     return total;
