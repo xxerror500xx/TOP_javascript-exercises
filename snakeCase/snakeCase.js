0f42044bda823604baf8a849a1ed91877b53c0ad
xxerror500xx <cliff.minks@gmail.com>
2018-02-11 11:54:36 -0600

    Add snake_case solution.

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,17 @@
-var snakeCase = function() {
 
-}
+const snakeCase = function(string) {
+  string = string.replace(/\.\./g, " ");
+  
+  if (!string.includes(" ")) {
+    string = string.replace(/([A-Z.])/g, " $&");
+  }
 
-module.exports = snakeCase
+  return string
+  .trim()
+  .toLowerCase()
+  .replace(/[,\?\.]/g, "")
+  .replace(/\-/g, " ")
+  .split(" ")
+  .join("_");
+};
+module.exports = snakeCase;
