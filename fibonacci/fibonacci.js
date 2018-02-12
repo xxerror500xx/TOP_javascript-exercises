ad7f7666791c78d426c9786a0790ea30b4f0bc05
xxerror500xx <cliff.minks@gmail.com>
2018-02-12 10:17:38 -0600

    Add Fib solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-var fibonacci = function() {
 
-}
+var fibonacci = function(num) {
+  let n1 = 0;
+  let n2 = 1;
+  if (num < 0) return "OOPS";
+  for (let i = 1; i < num; i++) {
+    var temp = n2;
+    n2 = n1 + n2;
+    n1 = temp;
+  }
+  return n2;
+};
 
 module.exports = fibonacci
