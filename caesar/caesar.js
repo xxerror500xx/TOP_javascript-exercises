f1b7b2bb54102b3bffd825d1e7545e6307cb0dc8
xxerror500xx <cliff.minks@gmail.com>
2018-02-12 09:07:31 -0600

    Add primitive Caesar solution

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,36 @@
-var caesar = function() {
+var caesar = function(string, shiftBy) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+  var splitStr = string.split('');
+  var newCypher = [];
+  var max = alphabet.length;
 
-}
+  var getShiftLetter = function(char, shiftBy) {
+    var index = alphabet.indexOf(char.toLowerCase());
+    var newIndex = index + shiftBy;
+    if (newIndex < 0) {
+      while (newIndex < 0) {
+        newIndex += max;
+      }
+    }
+    if (newIndex >= max) {
+      while (newIndex >= max) {
+        newIndex -= max;
+      }
+    }
+    var newChar = alphabet[newIndex];
+    if (char === char.toUpperCase()) newChar = newChar.toUpperCase();
+    return newChar;
+  };
 
-module.exports = caesar
+  for (var i = 0; i < splitStr.length; i++) {
+    if (alphabet.includes(splitStr[i].toLowerCase())) {
+      newCypher.push(getShiftLetter(splitStr[i], shiftBy));
+    } else {
+      newCypher.push(splitStr[i]);
+    }
+  }
+  return newCypher.join('');
+};
+
+
+module.exports = caesar;
